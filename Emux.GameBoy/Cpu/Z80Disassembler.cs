313ff55adf986727650049d33e2caecb39d07d21
Lee Fogg <leefogg@users.noreply.github.com>
2020-04-18 20:21:49 +0100

    Reuse the same Z80Instruction instance
    Avoid using `new`

--- Emux.GameBoy/Cpu/Z80Disassembler.cs
+++ Emux.GameBoy/Cpu/Z80Disassembler.cs
@@ -8,40 +8,36 @@ namespace Emux.GameBoy.Cpu
     public class Z80Disassembler 
     {
         private readonly GameBoyMemory _memory;
-        private ushort _position;
+        private readonly Z80Instruction _readInstruction = new Z80Instruction(0, Z80OpCodes.SingleByteOpCodes[0], new byte[0]);
 
         public Z80Disassembler(GameBoyMemory memory)
         {
             _memory = memory;
         }
 
-        /// <summary>
-        /// Gets or sets the position of the disassembler to read the next instruction from.
-        /// </summary>
-        public ushort Position
-        {
-            get { return _position; }
-            set { _position = value; }
-        }
+		/// <summary>
+		/// Gets or sets the position of the disassembler to read the next instruction from.
+		/// </summary>
+		public ushort Position { get; set; }
 
-        /// <summary>
-        /// Reads the next instruction from memory.
-        /// </summary>
-        /// <returns>The disassembled instruction.</returns>
-        public Z80Instruction ReadNextInstruction()
+		/// <summary>
+		/// Reads the next instruction from memory.
+		/// </summary>
+		/// <returns>The disassembled instruction.</returns>
+		public Z80Instruction ReadNextInstruction()
         {
-            ushort offset = _position;
-            byte code = _memory.ReadByte(_position++);
+            ushort offset = Position;
+            byte code = _memory.ReadByte(Position++);
 
             var opcode = code != Z80OpCodes.ExtendedTableOpcode
                 ? Z80OpCodes.SingleByteOpCodes[code]
-                : Z80OpCodes.PrefixedOpCodes[_memory.ReadByte(_position++)];
+                : Z80OpCodes.PrefixedOpCodes[_memory.ReadByte(Position++)];
 
-            var operands = _memory.ReadBytes(_position, opcode.OperandLength);
-            _position += (ushort)operands.Length;
+            var operands = _memory.ReadBytes(Position, opcode.OperandLength);
+            Position += (ushort)operands.Length;
 
-            var instruction = new Z80Instruction(offset, opcode, operands);
-            return instruction;
+            _readInstruction.Set(offset, opcode, operands);
+            return _readInstruction;
         }
     }
 }
